c00be2ce0928e539237905e7dae7fb75c09dfa43
SJcodeML <sidrajabin56@gmail.com>
2025-03-22 21:02:31 +0500

    code added

---
+++ Fast_api/simple_api/main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI 
+import random 
+
+
+app = FastAPI()
+
+
+side_hustles:list = [
+    "freelancing", 
+    "Digital Marketing ", 
+    "cryptocurrency trading ",
+    "investment"
+]
+
+
+money_quotes:list = [
+    "love the money ",
+    "liek te money ",
+    "hate the money ",
+     "i hate the money"  
+]
