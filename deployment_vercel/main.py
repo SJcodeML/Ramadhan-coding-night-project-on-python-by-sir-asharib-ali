2548c47654428d40986361273b928c9bd36ea35c
SJcodeML <sidrajabin56@gmail.com>
2025-03-24 11:33:31 +0500

    code added

---
+++ deployment_vercel/main.py
@@ -0,0 +1,4 @@
+# deployment of a api keys have done in a fast api project and the url are
+
+# https://ramadhan-coding-night-project-on-python-by-sir-asharib-ali.vercel.app/side_hustles?apikey=123456789
+# https://ramadhan-coding-night-project-on-python-by-sir-asharib-ali.vercel.app/money_quotes?apikey=123456789
